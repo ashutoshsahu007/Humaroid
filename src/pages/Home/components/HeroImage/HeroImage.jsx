9c21f3eeded8896eaac93f2d4c6730f6ea24399d
ashutosh sahu <ppaskumar@gmail.com>
2025-04-23 18:28:30 +0530

    add all

--- src/pages/Home/components/HeroImage/HeroImage.jsx
+++ src/pages/Home/components/HeroImage/HeroImage.jsx
@@ -5,7 +5,7 @@ const HeroImage = () => {
   return (
     <div className="h-[44vw] scrollbar-none">
       <Carousel>
-        <img src="hero/hero1.png" alt="..." />
+        {/* <img src="hero/hero1.png" alt="..." /> */}
         <img src="hero/img1.jpg" alt="..." />
         <img src="hero/img3.jpg" alt="..." />
       </Carousel>
